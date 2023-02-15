f544c1a7ef7b0ce383041c69b65e81bb16ff3b00
Dreamer-VD <danilavacun@gmail.com>
2023-02-15 23:00:58 +0300

    first commit

---
+++ Program/Program.cs
@@ -0,0 +1,51 @@
+﻿double x1, x2;
+try
+{
+    Console.WriteLine("Введите a");
+    double a = Convert.ToInt32(Console.ReadLine());
+    if (a == 0)
+    {
+        Console.WriteLine("Квадратичное уравнение не верно, повторите попытку");
+        Console.WriteLine("Для выхода нажмите любую клавишу...");
+        Console.ReadKey(true);
+        return;
+    }
+Console.WriteLine("Введите b");
+double b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите c");
+double c = Convert.ToInt32(Console.ReadLine());
+double d = Math.Pow(b, 2)-4*a*c;
+if (d < 0)
+{
+    Console.WriteLine("Квадратное уравнение не имеет корней");
+}
+else
+{
+    if (d > 0)
+    {
+        x1 = (-b + Math.Sqrt(d)) / (2 * a);
+        x2 = (-b - Math.Sqrt(d)) / (2 * a);
+        Console.WriteLine("X1={0:F1} и X2={1:F1}", x1, x2);
+    }
+    if (d == 0)
+    {
+        x1 = -b / (2 * a);
+        x2 = x1;
+        if (x2==0)
+        {
+            x2= Math.Abs(x2);
+            Console.WriteLine("X={0:F1}", x2);
+        }
+        else
+        {
+            Console.WriteLine("X={0:F1}", x2);
+        }
+    }
+}
+Console.WriteLine("Для выхода нажмите любую клавишу...");
+Console.ReadKey(true);
+}
+catch (Exception e)
+{
+    Console.WriteLine("ОШИБКА");
+}
